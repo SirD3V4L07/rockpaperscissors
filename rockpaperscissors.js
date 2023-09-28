9b6233d63b29a6f328c096955f634061cfe7c760
SirD3V4L07 <ascendantspiral@gmail.com>
2023-09-28 16:04:54 +0100

    Totally didn't forget to commit changes

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -7,148 +7,75 @@ let playerMatchCount = 0;
 let comMatchCount = 0;
 
 
+const rockButton = document.querySelector("#rock");
+const paperButton = document.querySelector("#paper");
+const scissorsButton = document.querySelector("#scissors");
+const roundResult = document.querySelector("#roundResult");
 
-function getUserAnswer() {
-    do {
-        userAnswer = prompt("What is your choice?");
-        convertUserAnswer(userAnswer);
-        
-        if (userAnswer != "Rock" && userAnswer != "Paper" && userAnswer != "Scissors") {
-            alert("Invalid option! Try again.");
-        }
-    } while (userAnswer != "Rock" && userAnswer != "Paper" && userAnswer != "Scissors");
+rockButton.addEventListener("click", function(){game("Rock");});
+paperButton.addEventListener("click", function(){game("Paper");});
+scissorsButton.addEventListener("click", function(){game("Scissors");});
+
+function game(userAnswer) {
+    /**
+     * Generate COM answer
+     * Display round's result
+     * Display total rounds score
+     * Once 5 rounds are reached, display game outcome
+     * Display games won and lost
+     * Display "choose an option to start a new game"
+     * Start a new game if an option is clicked again
+     */
     
-    console.log("User answer is " + userAnswer);
-}
+    getOutcome(userAnswer, getComAnswer());
 
-function convertUserAnswer(userChoice) {
-    /* Convert first character to uppercase */
-    let firstCharacter = userChoice.slice(0,1);
-    firstCharacter = firstCharacter.toUpperCase();
-
-    /* Convert remaining characters to lowercase */
-    let remainingCharacters = userChoice.slice(1, userChoice.length);
-    remainingCharacters = remainingCharacters.toLowerCase();
-
-    /* Put together final result, capitalized string */
-    userChoice = firstCharacter + remainingCharacters;
-    userAnswer = userChoice;
-}
-
-function convertPlayAgainAnswer() {
-    /* Convert first character to uppercase */
-    let firstCharacter = playAgain.slice(0,1);
-    firstCharacter = firstCharacter.toUpperCase();
-
-    /* Convert remaining characters to lowercase */
-    let remainingCharacters = playAgain.slice(1, playAgain.length);
-    remainingCharacters = remainingCharacters.toLowerCase();
-
-    /* Put together final result, capitalized string */
-    playAgain = firstCharacter + remainingCharacters;
 }
 
 
 function getComAnswer() {
     computerAnswer = Math.floor(Math.random() * 3) + 1;
     if (computerAnswer == 1)  {
-        computerAnswer = "Rock";
-    } else if (computerAnswer == 2) {
-        computerAnswer = "Paper";
-    } else if (computerAnswer == 3) {
-        computerAnswer = "Scissors";
-    }
+            computerAnswer = "Rock";
+        } else if (computerAnswer == 2) {
+            computerAnswer = "Paper";
+        } else if (computerAnswer == 3) {
+            computerAnswer = "Scissors";
+        }
     console.log("Computer answer is " + computerAnswer);
-}
+    return computerAnswer;
+
+ }
 
 
 function getOutcome(userChoice, computerChoice) {
     /* First checks for draw, which takes care of 3 out of 9 possible outcomes */
 
     if (userChoice == computerChoice) {
-        alert("Draw! Try again.");
+        displayRoundResult("Draw! Try again.");
         console.log("Entered draw state");
     } else {
 
         /* Checks for the remaining possible outcomes */
         switch (userChoice) {
             case "Rock": switch (computerChoice) {
-                case "Paper": {alert("You lose!"); comRoundScore += 1; break;}
-                case "Scissors": {alert("You win!"); playerRoundScore += 1; break;}
+                case "Paper": {displayRoundResult("You lose!"); comRoundScore += 1; break;}
+                case "Scissors": {displayRoundResult("You win!"); playerRoundScore += 1; break;}
             }; break;
             
             case "Paper": switch (computerChoice) {
-                case "Rock": {alert("You win!"); playerRoundScore += 1; break;}
-                case "Scissors": {alert("You lose!"); comRoundScore += 1; break;}
+                case "Rock": {displayRoundResult("You win!"); playerRoundScore += 1; break;}
+                case "Scissors": {displayRoundResult("You lose!"); comRoundScore += 1; break;}
             }; break;
 
             case "Scissors": switch (computerChoice) {
-                case "Rock": {alert("You lose!"); comRoundScore += 1; break;}
-                case "Paper": {alert("You win!"); playerRoundScore += 1; break;}
+                case "Rock": {displayRoundResult("You lose!"); comRoundScore += 1; break;}
+                case "Paper": {displayRoundResult("You win!"); playerRoundScore += 1; break;}
             }; break;
         }
     }
     
 }
 
-function game() {
-    let roundCounter = 1;    
-    
-    
-    console.log("=== NEW GAME ===");
-    playerRoundScore = 0;
-    comRoundScore = 0;
-
-   // do {
-        console.log("Round " + roundCounter + "!");
-        console.log("Choose an option!");
-        console.log("| Rock | - | Paper | - | Scissors |");
-        getUserAnswer();   
-        getComAnswer();    
-        getOutcome(userAnswer, computerAnswer); 
-        console.log("Current score:");
-        console.log("Player: " + playerRoundScore);
-        console.log("COM: " + comRoundScore);
-       // roundCounter += 1;
-   // } while (roundCounter < 6);
-
-    /**
-     * Resolve match winner
-     */
-
-  /*  if (playerRoundScore > comRoundScore) {
-        console.log("YOU WIN THE MATCH!");
-        alert("YOU WIN THE MATCH!");
-        playerMatchCount += 1;
-    } else if (comRoundScore > playerRoundScore) {
-        console.log("YOU LOSE");
-        alert("YOU LOSE!");
-        comMatchCount += 1;
-    } else {
-        console.log("It's a draw!");
-    }
-
-    console.log("Matches Won:");
-    console.log("Player: " + playerMatchCount);
-    console.log("COM: " + comMatchCount);*/
-
-
+function displayRoundResult (resultString) {
+    roundResult.textContent = resultString;
 }
-
-
-do {
-    game();    
-
-    do {
-        playAgain = prompt("Would you like to play again? (Yes/No)");
-        convertPlayAgainAnswer();
-        
-        if (playAgain != "Yes" && playAgain != "No") {
-            alert("Invalid option! Try again.");
-        }
-    } while (playAgain != "Yes"  && playAgain != "No");
-
-    
-} while (playAgain == "Yes");
-
-console.log("Thanks for playing!");
